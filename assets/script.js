b4f8a0161e88ed55e4e8e5e6b7a34cd3b246bf2e
adrianskelton <>
2023-08-22 08:27:02 +0000

    add modal for rules button not yet complete

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,35 @@
 console.log('hello');
+function vsImageOver() {
+        document.getElementById("vs-image").classList.add("vs");
+        document.getElementById("vs-image").classList.remove("vs2");
+    }
+    
+    function vsImageOut() {
+        const dropdowns = document.querySelectorAll(".dropdown");
+        dropdowns.forEach((dropdown) => {
+            dropdown.classList.add("vs2");
+            dropdown.classList.remove("vs");
+        });
+    }
 
-function changeClass() {
-    document.getElementById("vs-image").className = "vs2";
+//Popup Modal Script
+
+var modal = document.querySelector(".modal");
+var trigger = document.querySelector(".trigger");
+var closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
 }
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
+
+//End of Modal Script
